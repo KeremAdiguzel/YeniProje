75b6cb5dc211d94c01be6ae892e6bade380b35eb
KeremAdiguzel <vektorel@LAB3-107>
2016-01-02 13:13:37 +0200

    for

--- Javaprojesi/src/com/kerem/ders/deneme/DersJavaClass.java
+++ Javaprojesi/src/com/kerem/ders/deneme/DersJavaClass.java
@@ -4,8 +4,15 @@ public class DersJavaClass {
 
 	public static void main(String[] args) {
 		System.out.println("Merhaba Dünya");
-		int a=1;
-		System.out.println(a);
-	}
+		int a=1,b;
+		for(a=1;a<=5;a++)
+		{
+			System.out.println(a);
+		for(b=1;b<=a;b++)
+		{
+			System.out.print("*");
+		}
+		}
+		}
 
 }
